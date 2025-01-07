ca95928166e5b6f81733fddcd2c069c15d533bf0
PetoKrisa <petokrisa2006@gmail.com>
2025-01-07 20:56:32 +0100

    path fix

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ app.use("/", express.static("./"))
 app.get("/", (req,res)=>{
     res.sendFile(`${__dirname}/canvas.html`)
 })
-app.get("/style.css", (req,res)=>{
-    res.sendFile(`${__dirname}/style.css`)
-})
 
 app.listen(PORT, ()=>{
     console.log(`Server Listening: 'http://localhost:${PORT}'`)
